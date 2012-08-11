6c98eaa6ec4d21d824620dccf1c3ccf2bf84b914
Dmitriy Sukharev <sukharevd@gmail.com>
2012-08-11 23:50:25 +0300

    Moved pomodoro logic to separate class.

---
+++ pomodoro.cpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2012 Dmitriy Sukharev.
+ ** All rights reserved.
+ ** Contact: Dmitriy Sukharev (ecuna@mail.ru)
+ **
+ ** This file is part of Pomodoro Timer.
+ **
+ ** Pomodoro Timer is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Pomodoro Timer is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Pomodoro Timer.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+
+#include "pomodoro.h"
+
+Pomodoro::Pomodoro(QObject *parent) :
+    QObject(parent)
+{
+    time_left = 0;
+    timer = new QTimer();
+    timer->setInterval(1000);
+    connect(timer, SIGNAL(timeout()), this, SLOT(timer_ticked()));
+}
+
+Pomodoro::~Pomodoro()
+{
+    delete timer;
+}
+
+void Pomodoro::startPomodoro()
+{
+    timer->stop();
+    time_left = 25 * 60;
+    timer->start();
+}
+
+void Pomodoro::startShortBreak()
+{
+    timer->stop();
+    time_left = 5 * 60;
+    timer->start();
+}
+
+void Pomodoro::startLongBreak()
+{
+    timer->stop();
+    time_left = 30 * 60;
+    timer->start();
+}
+
+void Pomodoro::pause()
+{
+    timer->stop();
+}
+
+void Pomodoro::resume()
+{
+    timer->start();
+}
+
+void Pomodoro::timer_ticked()
+{
+    time_left--;
+    if (!time_left) {
+        timer->stop();
+        emit timeout();
+    }
+    emit tick();
+}
+
+time_t Pomodoro::getTimeLeft()
+{
+    return time_left;
+}
+
+bool Pomodoro::isActive()
+{
+    return timer->isActive();
+}
---
+++ pomodoro.h
@@ -0,0 +1,60 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2012 Dmitriy Sukharev.
+ ** All rights reserved.
+ ** Contact: Dmitriy Sukharev (ecuna@mail.ru)
+ **
+ ** This file is part of Pomodoro Timer.
+ **
+ ** Pomodoro Timer is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Pomodoro Timer is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Pomodoro Timer.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+
+#ifndef POMODORO_H
+#define POMODORO_H
+
+#include <QObject>
+#include <QTimer>
+#include <time.h>
+
+class Pomodoro : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Pomodoro(QObject *parent = 0);
+    ~Pomodoro();
+
+    void startPomodoro();
+    void startShortBreak();
+    void startLongBreak();
+    void pause();
+    void resume();
+
+    time_t getTimeLeft();
+    QTimer* getTimer();
+    bool isActive();
+
+signals:
+    void tick();
+    void timeout();
+
+public slots:
+    void timer_ticked();
+
+private:
+    time_t time_left;
+    QTimer* timer;
+};
+
+#endif // POMODORO_H
